7f07b7aca1e09af5bbf1e4afa0a5821fbabe9714
mohd-sadab-au3 <sadabkhan14198@gmail.com>
2019-12-14 07:14:58 +0530

    react heroku

---
+++ build/precache-manifest.d5d5fd646aff855e500c82170c404388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc63daa2b3c53ed92350567ae5a0feca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb2175917ae1705b876",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b52081a8a6bc5f24bbd2",
+    "url": "/static/js/2.df5efe12.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.df5efe12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fb2175917ae1705b876",
+    "url": "/static/js/main.ae03ed80.chunk.js"
+  },
+  {
+    "revision": "871fb92fc64991868fdd",
+    "url": "/static/js/runtime-main.7607cef0.js"
+  }
+]);
